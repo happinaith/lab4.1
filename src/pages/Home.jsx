6acc95639d795a1ef52b52aed3fb3ce1c2e1395c
happinaith <90930614+happinaith@users.noreply.github.com>
2023-11-25 10:47:34 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,3 @@
-import { Button, Center, Text, Stack } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
-
-export const Home = () => (
-    <Center h="100vh">
-        <Stack>
-            <Text textAlign="center">Старница Home</Text>
-            <Button as={Link} to="/asdfg" colorScheme="blue">
-                Перейти на страницу 404
-            </Button>
-        </Stack>
-    </Center>
-)
-
 import React, { useState } from 'react'
 import {
     chakra,
@@ -115,4 +101,3 @@ export const Home = () => {
         </Flex>
     )
 }
-
